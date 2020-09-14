7ab1bb9d63272e105c2f8377c37033e5c75dc28f
leablogs <shilh123@sina.cn>
2020-09-14 11:52:54 +0800

    初始化系统代码

---
+++ src/test/java/com/leablogs/WechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leablogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
